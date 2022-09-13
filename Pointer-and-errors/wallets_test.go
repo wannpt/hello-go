9c06ee14ffbf3813cc241b38dc0c93b6be06c9c2
wannpt <naphatthorn.k@gmail.com>
2022-09-14 00:59:27 +0700

    asserting error

--- Pointer-and-errors/wallets_test.go
+++ Pointer-and-errors/wallets_test.go
@@ -4,34 +4,47 @@ import (
 	"testing"
 )
 
+
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-	
-		got := wallet.Balance()
-		expected := Bitcoin(10)
-	
-		if got != expected {
-			t.Errorf("got %s but expected %d", got, expected)
+	assertBalance := func(t testing.TB, w Wallet, want Bitcoin){
+		t.Helper()
+		got := w.Balance()
+		if got != want { 
+			t.Errorf("got %s but expected %d", got, want)
 		}
+	}
+	
+	assertError := func(t testing.TB, err error){
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+	
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, 10)
 	})
 
 	t.Run("Withdraw", func(t *testing.T){
-
 		wallet := Wallet{balance: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, 10)
 
-		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-		expected := Bitcoin(10)
-
-		if got != expected {
-			t.Errorf("got %s but expected %s", got, expected)
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
 		}
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T){
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
+		assertBalance(t, wallet, startingBalance)
 
 	})
 
