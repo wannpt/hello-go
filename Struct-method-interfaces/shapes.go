649da20b3e8a4a601d9be5bb7f16c7d5bbadab51
wannpt <naphatthorn.k@gmail.com>
2022-09-13 23:52:35 +0700

    test table

--- Struct-method-interfaces/shapes.go
+++ Struct-method-interfaces/shapes.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.radius * c.radius
 }
 
+type Triangle struct {
+	width float64
+	height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.width * t.height) * 0.5
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 
 	return 2 * (rectangle.width + rectangle.height)
--- Struct-method-interfaces/shapes_test.go
+++ Struct-method-interfaces/shapes_test.go
@@ -14,23 +14,20 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		name string
+		shape Shape
+		want float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, want: 72},
+		{name: "Circle",shape: Circle{10}, want: 314.1592653589793},
+		{name: "Triangle",shape: Triangle{12, 6}, want: 36.0},
+  }
+
+		for _, tt := range areaTests {
+			got := tt.shape.Area()
+				if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-
-	t.Run("Rectangle Area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
-
-	})
-
-	t.Run("Circle Area", func(t *testing.T){
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
-
 }
